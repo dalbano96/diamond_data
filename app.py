3e73dd294ff27ab61885e1cbf135b8e60edf7efc
dalbano96 <dalbano96@gmail.com>
2023-05-11 22:29:30 -1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+
+def get_score_from_metacritic():
+    print()
+
+def put_score_in_notion():
+    print()
+
+def get_hours_from_hltb():
+    print()
+
+def put_hours_in_notion():
+    print()
+
+
+'''
+Main functions
+'''
+def import_scores():
+    print()
+
+def import_hours():
+    print()
